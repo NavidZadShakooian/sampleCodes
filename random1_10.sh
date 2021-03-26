05e7bac488a1ddc69df7c803af112d33cef17a8e
NavidZadShakooian <zshnavid@gmail.com>
2021-03-26 11:52:11 +0430

    comment 2

--- random1_10.sh
+++ random1_10.sh
@@ -7,10 +7,13 @@ ITER=10
 function do_iter(){
 counter=0
 
+#Main loop
 while [ $counter -lt 10 ]
 do
+	#generate a random number between 1-10
 	tmp=$(( $RANDOM % 10 + 1))
 	
+	#check to not print if number has been showned once
 	if [[ ! ${ARR[$tmp]} -eq  1 ]]
   then
   	ARR[$tmp]=1
