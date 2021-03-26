f929509793a4a2c1f125dea905039c702274e581
NavidZadShakooian <zshnavid@gmail.com>
2021-03-27 00:13:30 +0430

    comment 2

--- random1_10.sh
+++ random1_10.sh
@@ -2,27 +2,33 @@
 
 #Number of iteration
 ITER=10
+#ARR      array with lenght of 10 to prevent a repeated number output
+#counter  loop counter
+#do_iter  function
 
 #Function generates numbers from 1 to 10 randomly and without any repeated number
 function do_iter(){
 counter=0
 
 #Main loop
-while [ $counter -lt 10 ]
+while [ $counter -lt $ITER ]
 do
 	#generate a random number between 1-10
 	tmp=$(( $RANDOM % 10 + 1))
 	
-	#check to not print if number has been showned once
+	#check to not print the number if it has been printed befor
 	if [[ ! ${ARR[$tmp]} -eq  1 ]]
-  then
-  	ARR[$tmp]=1
-	((counter+=1))
-	echo -n  $tmp"  "
-  fi
-done
+        then
+		#sets corresponding array member to 1
+	  	ARR[$tmp]=1
+		#increment loop counter
+		((counter+=1))
 
+		echo -n  $tmp"  "
+        fi
+done
 }
+
 
 do_iter
 echo ""
